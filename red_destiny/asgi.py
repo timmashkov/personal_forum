5a2bb0c3749237a251c8f001122feb599adc0707
timmashkov <bodomizer#yandex.ru>
2024-01-04 12:50:43 +0300

    the project creation

---
+++ red_destiny/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for red_destiny project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "red_destiny.settings")
+
+application = get_asgi_application()
